bb143409573ccbb99ca064c8291ad4b614fc76b5
Urtzi <urtzi.maruri.48440@ikasle.egibide.org>
2025-11-05 12:22:17 +0100

    Urtzi: agrega su funcion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Proyecto colaborativo de 4 alumnos iniciado.");
+    public static void main(String[] args) {System.out.println("Proyecto colaborativo de 4 alumnos iniciado.");
+    }
+    public static void funcionUrtzi() {
+        System.out.println("Función desarrollada por Urtzi");
     }
 }
