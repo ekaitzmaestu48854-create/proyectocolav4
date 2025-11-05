3a246be3fa7d5f68b35903dc223b21608506279b
Aritz <aritz.aragunde.50464@ikasle.egibide.org>
2025-11-05 12:20:36 +0100

    AritzA agrega su funcion

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Proyecto colaborativo de 4 alumnos iniciado.");
     }
+
+    // AritzA
+    public static void funcionAlumno1() {
+        System.out.println("Función desarrollada por AritzA");
+    }
+
 }
