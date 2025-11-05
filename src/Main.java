e78b459e94dcd1477b11c9491608735024406de4
ekai2 <ekaitz.maestu.48854@ikasle.egibide.org>
2025-11-05 12:17:21 +0100

    Ekaitz: agrega su funcion

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Proyecto colaborativo de 4 alumnos iniciado.");
     }
+    // ekaitz
+    public static void funcionekaitz() {
+        System.out.println("Función desarrollada por ekaitz");
+    }
 }
