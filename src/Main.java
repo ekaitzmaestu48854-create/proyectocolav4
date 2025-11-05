7436a21f800a3ebe6402268d55491f94983ec2aa
Jon <Jon.buesa.41441@ikasle.egibide.org>
2025-11-05 12:06:43 +0100

    primer commit jon

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Proyecto colaborativo de 4 alumnos iniciado.");
     }
+    public static void funcionJon(){
+        System.out.println("funcion creada por Jon");
+    }
 }
