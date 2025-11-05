5db3092d39201f47dcda8c1c0f1f857dd8a3774e
Unai <unai.castellano.47462@ikasle.egibide.org>
2025-11-05 12:11:17 +0100

    Unai: agrega su función

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Proyecto colaborativo de 4 alumnos iniciado.");
     }
+    // Unai
+    public static void funcionUnai() {
+        System.out.println("Función desarrollada por Unai");
 }
